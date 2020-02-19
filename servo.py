2219149c0146976bd2e8a68f300cbcc6e3b56523
pi <pi>
2020-02-19 12:25:26 +0000

    servo and stepper motor modules are updated and more precise. Calibration is done

--- servo.py
+++ servo.py
@@ -15,24 +15,42 @@ class ServoMotor:
 
     def _create_full_angle_list(self):
         angle_list = [  [0, 510],
-                        [45, 800],
-                        [90, 1300],
-                        [135, 1850]]
+                        [18, 550],
+                        [36, 750],
+                        [54, 960],
+                        [72, 1100],
+                        [90, 1310],
+                        [108, 1510],
+                        [126, 1680],
+                        [144, 1900]]
+        """
+        [45, 1000],
+        [90, 1500],
+        [135, 2000]]
+        """
 
         np_angle_list = np.array(angle_list)
+        pulsewidth_list = np_angle_list[:,1]
 
-        angles_to_pw = np.poly1d(
-            np.polyfit(
-                np_angle_list[:, 0],
-                np_angle_list[:, 1],
-                3
-            )
-        )
+        for i in range(len(angle_list)-1):
+            smaller_angle = angle_list[i][0]
+            smaller_angle_pw = angle_list[i][1]
+            larger_angle = angle_list[i+1][0]
+            larger_angle_pw = angle_list[i+1][1]
+            angle_diff = larger_angle - smaller_angle
+            pw_diff_per_deg = (larger_angle_pw - smaller_angle_pw) / angle_diff
+            
+            for j in range(angle_diff):
+                self._full_angle_list.append([smaller_angle + j, int(smaller_angle_pw + pw_diff_per_deg * j)])
+                
 
         # construct full angle list
-        for angle in range(136):
-            self._full_angle_list.append([angle, int(angles_to_pw(angle))])
+        #for angle in range(136):
+        #    self._full_angle_list.append([angle, int(angles_to_pw(angle))])
 
+        #self._full_angle_list[0] = [0, 510]
+        #self._full_angle_list[1] = [1, 510]
+        #self._full_angle_list[2] = [2, 510]
         #print(self._full_angle_list)
 
     def _setAngleSetup(self):
@@ -40,8 +58,8 @@ class ServoMotor:
         self._rpi.set_servo_pulsewidth(self._PWM_PIN, 0)
         
     
-    def setAngle(self,angle, sleep_time = 0.02):
-        assert(angle <= 135 and angle >= 0)
+    def setAngle(self,angle, sleep_time = 0.05):
+        assert(angle <= 144 and angle >= 0)
         angle = int(angle)
         pulse_width = self._full_angle_list[angle][1]
         
--- stepper.py
+++ stepper.py
@@ -6,7 +6,9 @@ class StepperMotor:
     def __init__(self):
         self.pin1,self.pin2,self.pin3,self.pin4 =  (5,7,31,29)        
         self.current_angle = 0
-        self._ONE_STEPPER_ANGLE = 360/50 # full circle divided by number of steps that stepper can made in a full circle
+        self._ONE_STEPPER_ANGLE = 360/400 # full circle divided by number of steps that stepper can made in a full circle
+        self._substep_forwards = 0
+        self._substep_backwards = 0
         self._setup()
 
     def _setup(self):
@@ -17,32 +19,55 @@ class StepperMotor:
         GPIO.setup(self.pin4, GPIO.OUT)
 
     def forward_step(self):
-        self.set_stepper(1,0,1,0)
-        self.set_stepper(0,1,1,0)
-        self.set_stepper(0,1,0,1)
-        self.set_stepper(1,0,0,1)
+        if self._substep_forwards == 0:
+            self.set_stepper(1,0,1,0)
+            self._substep_forwards += 1
+        elif self._substep_forwards == 1:
+            self.set_stepper(0,1,1,0)
+            self._substep_forwards += 1
+        elif self._substep_forwards == 2:
+            self.set_stepper(0,1,0,1)
+            self._substep_forwards += 1
+        else:
+            self.set_stepper(1,0,0,1)
+            self._substep_forwards = 0
         
     def backward_step(self):
-        self.set_stepper(1,0,0,1)
-        self.set_stepper(0,1,0,1)
-        self.set_stepper(0,1,1,0)
-        self.set_stepper(1,0,1,0)
+        if self._substep_backwards == 0:
+            self.set_stepper(1,0,0,1)
+            self._substep_backwards += 1
+        elif self._substep_backwards == 1:
+            self.set_stepper(0,1,0,1)
+            self._substep_backwards += 1
+        elif self._substep_backwards == 2:
+            self.set_stepper(0,1,1,0)
+            self._substep_backwards += 1
+        else:
+            self.set_stepper(1,0,1,0)
+            self._substep_backwards = 0
+        
 
     def set_stepper(self,in1,in2,in3,in4):
         GPIO.output(self.pin1, in1)
         GPIO.output(self.pin2, in2)
         GPIO.output(self.pin3, in3)
         GPIO.output(self.pin4, in4)
-        sleep(0.01)
+        sleep(0.02)
 
     def transform_angle_to_steps(self,angle= 0):
-        steps = int(angle * 50 /360) # it is the same as ( angle / self._ONE_STEPPER_ANGLE )
+        steps = int(angle * 400 /360) # it is the same as ( angle / self._ONE_STEPPER_ANGLE )
         return steps
 
     def setAngle(self,angle):
+        #new_angle = self.current_angle + angle
+        #print("angle is %d" % angle)
+        #print("new_angle is %d" % new_angle)
+        assert(angle < 90 and angle > -110)
+               
         angle_to_move = angle - self.current_angle
         if(angle_to_move >= 0):      
             steps = self.transform_angle_to_steps(angle_to_move)
+            #print(steps)
             for step in range(steps):
                 self.backward_step()
                 self.current_angle += self._ONE_STEPPER_ANGLE
@@ -54,8 +79,6 @@ class StepperMotor:
                 self.current_angle -= self._ONE_STEPPER_ANGLE
 
 
-
-
     # this method is never called. This an example of what should one movement represent
     def move(self, angle):
         self._setup()
@@ -63,8 +86,16 @@ class StepperMotor:
         GPIO.cleanup()
 
     def _reset_position(self):
+        self._setup()
         self.setAngle(0)
+        GPIO.cleanup()
     
     def __del__(self):
         self._reset_position()
-        GPIO.cleanup()
+        
+        # Hack to stop cleanup from turning off the internal pull ups
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.pin2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.pin3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.pin4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
